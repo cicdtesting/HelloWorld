7a1e55539218b07519be185a1420068c1cc05e0d
cicdtesting <sreenivasulu.reddy@intellectdesign.com>
2019-08-23 16:03:02 +0530

    New Project

---
+++ IamHere/src/com/intellect/main/test/TestJava.java
@@ -0,0 +1,11 @@
+package com.intellect.main.test;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello there!");
+
+	}
+
+}
